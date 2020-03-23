a34c1af68cbe26803b19aae302da9dbc0677215b
mdashmiller <m.robert.miller@gmail.com>
2020-03-23 16:42:53 -0500

    add intersection observer

--- src/Headlines.tsx
+++ src/Headlines.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 
 interface Story {
   id: number,
@@ -15,15 +15,44 @@ interface Props {
   loadingMoreError: boolean
 }
 
+const handleIntersect: IntersectionObserverCallback = entries => {
+  const target = entries[0]
+
+  // log message to console when last headline in the
+  // list appears on screen
+  if (target.isIntersecting) {
+    console.log('Last item visible!!!')
+  }
+}
+
 export const Headlines: React.FC<Props> = ({
-  storyData: stories,
+  storyData,
   initialLoading,
   initialLoadError,
   fetchMoreStories,
   loadingMore,
   loadingMoreError
 }) => {
+  const endOfList = useRef<HTMLLIElement>(null)
   const buttonText = loadingMore ? 'Loading...' : 'Load More Stories'
+
+  // each time more headlines are rendered change the endOfList
+  // ref to refer to the last headline on screen
+  useEffect(() => {
+    // create an observer to watch for when the final
+    // headline is scrolled into view
+    const observer = new IntersectionObserver(handleIntersect)
+
+    // when the list of headlines has rendered, the final
+    // headline will be stored in the endOfList ref and
+    // observed by the Intersection Observer API
+    if (endOfList && endOfList.current) {
+      observer.observe(endOfList.current)
+    }
+
+    // unobserve on willUnmount
+    return () => observer.disconnect()
+  }, [storyData.length])
 
   if (initialLoading) return <div data-testid="initial-loading">Loading...</div>
 
@@ -32,13 +61,27 @@ export const Headlines: React.FC<Props> = ({
   return (
     <div>
       <ul>
-        {!!stories && stories.map(story =>
-          <li data-testid={`headline-${story.id}`} key={story.id}>
-            <a href={story.url} target="_blank" rel="noopener noreferrer">
-              {story.title}
-            </a>
-          </li>
-        )}
+        {!!storyData && storyData.map((story, index) => {
+          // return an li with endOfList ref only if it
+          // is the last item in the storyData array
+          if (index === storyData.length - 1) {
+            return (
+              <li data-testid={`headline-${story.id}`} key={story.id} ref={endOfList}>
+                <a href={story.url} target="_blank" rel="noopener noreferrer">
+                  {story.title}
+                </a>
+              </li>
+            )
+          } else {
+            return (
+              <li data-testid={`headline-${story.id}`} key={story.id}>
+                <a href={story.url} target="_blank" rel="noopener noreferrer">
+                  {story.title}
+                </a>
+              </li>
+            )
+          }
+        })}
       </ul>
       {loadingMoreError ? (
         <div data-testid="load-more-error">
